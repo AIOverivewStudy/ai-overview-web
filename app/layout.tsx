a26a624b9e71667145f5bf3142c6cfb6db167604
xiaotianxt <me@xiaotian.dev>
2025-09-13 22:26:16 -0400

    feat: posthog integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,23 @@
-'use client'
-import type React from "react"
-import "./globals.css"
-import { AnalyticsTracker } from "@/components/analytics-tracker"
-import { Suspense } from "react"
+"use client";
+import type React from "react";
+import "./globals.css";
+import { AnalyticsTracker } from "@/components/analytics-tracker";
+import { Suspense, useEffect } from "react";
+import { usePostHog } from "posthog-js/react";
+import { useSearchParams } from "next/navigation";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const posthog = usePostHog();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const research_id = searchParams.get("RID") || "Anonymous user";
+    posthog.identify(research_id);
+  }, [posthog, searchParams]);
   return (
     <html lang="en">
       <body>
@@ -12,5 +25,5 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Suspense>{children}</Suspense>
       </body>
     </html>
-  )
+  );
 }
